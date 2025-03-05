63e74e57b86990229fb31b01a69980fbf5129ad9
Rostislav <mosijcukrostislav7@gmail.com>
2025-03-05 10:14:41 +0200

    Виконано завдання 3 і 4

--- lab4/autoload.php
+++ lab4/autoload.php
@@ -1,17 +1,10 @@
 <?php
-spl_autoload_register(function ($className) {
-    $directories = [
-        __DIR__ . "/Controllers/",
-        __DIR__ . "/Models/",
-        __DIR__ . "/Views/",
-    ];
+spl_autoload_register(function ($class) {
+    $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
 
-    foreach ($directories as $directory) {
-        $file = $directory . $className . ".php";
-        if (file_exists($file)) {
-            require_once $file;
-            return;
-        }
+    $file = __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
     }
 });
-?>
--- lab4/index.php
+++ lab4/index.php
@@ -8,4 +8,23 @@ include_once("Views/UserView.php");
 // Task 2
 echo "<h2>Task 2</h2>";
 include_once("./autoload.php");
-$userView = new UserView("Rostyslav", 25);
+// $userView = new UserView("Rostyslav", 25);
+
+// Task 3 and 4
+echo "<h2>Task 3 and 4</h2>";
+use Controllers\UserController;
+use Models\UserModel;
+use Views\UserView;
+
+$controller = new UserController();
+$controller->set_name("car");
+echo "<span>Name controller: </span>" . $controller->get__name();
+
+$view = new UserView("Rostyslav", 19);
+
+$model = new UserModel();
+$model->set_info("model2", "sky");
+echo "<br />";
+echo "<span>Name: </span>" . $model->get__name();
+echo "<br />";
+echo "Type: " . $model->get__type();
